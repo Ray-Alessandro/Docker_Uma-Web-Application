287aa9103f751bb98bfd5eb73ea87ff9840c8371
Jenn Espinoza <jenniespinoza2002@hotmail.com>
2023-11-19 12:56:30 -0500

    feat: add formdata

--- Uma-Backend/src/database.js
+++ Uma-Backend/src/database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 //entorne de produccion, cuando hablen los contenedores
 //mongoose.connect("mongodb://database/Uma_Database")
-mongoose.connect("mongodb://localhost:27019/Uma_Database")
+//mongoose.connect("mongodb://localhost:27019/Uma_Database")
+mongoose.connect("mongodb://localhost:27017/Uma_Database")
 .then
 (
     db => console.log("DB is connected to", db.connection.host)
