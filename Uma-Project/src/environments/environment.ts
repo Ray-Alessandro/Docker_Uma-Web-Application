01defd628de92b6b7cfe3d20d33632ff05659ee8
Ray <ray_punkeki@hotmail.com>
2023-11-24 23:13:07 -0500

    feat: final project  v1.0

--- Uma-Project/src/environments/environment.ts
+++ Uma-Project/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     development: false,
     baseURL: '/uma-backend-config'
-    //baseURL: 'https://n4b9k1n1-3000.brs.devtunnels.ms'
+    //baseURL: 'http://localhost:5000'
 };
